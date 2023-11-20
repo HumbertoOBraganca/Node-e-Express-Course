8b2659a86784df5296e5a7c00d040382378c7436
Makako-Loko <Makako-Loko@users.noreply.github.com>
2023-11-20 11:12:45 -0300

    Commit teste

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const EventEmitter = require('events');
 
 const customEmitter = new EventEmitter()
 
+customEmitter.on('response',(name,id)=>{
+    console.log(`data recieved - user ${name} with id:${id}`)
+})
 customEmitter.on('response',()=>{
-    console.log(`data recieved`)
+    console.log(`some other logic`)
 })
 
-customEmitter.emit('response')
+customEmitter.emit('response', 'Humberto', 149459)
